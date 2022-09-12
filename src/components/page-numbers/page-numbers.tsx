561f92d9167c836df8fd1e12ef95a469c56a9f2e
reyolan <reynardolan@gmail.com>
2022-09-12 20:53:56 +0800

    add initial implementation of usePagination hook

--- src/components/page-numbers/page-numbers.tsx
+++ src/components/page-numbers/page-numbers.tsx
@@ -10,17 +10,15 @@ interface IPageNumbersProps {
 
 function PageNumbers({ data, setPaginatedData }: IPageNumbersProps) {
   const {
-    setDataToPaginate,
     currentPageData,
     firstPage,
     lastPage,
     nextPage,
     prevPage,
+    jumpToPage,
     pageRange,
     currentPage,
-  } = usePagination<IMarketData>(10, 1, 5);
-
-  useEffect(() => setDataToPaginate(data), []);
+  } = usePagination<IMarketData>(10, 1, 5, data);
 
   useEffect(() => {
     setPaginatedData(currentPageData);
@@ -29,18 +27,35 @@ function PageNumbers({ data, setPaginatedData }: IPageNumbersProps) {
   return (
     <PageNumbersContainer>
       {currentPage > 1 && <div onClick={prevPage}>Previous</div>}
-      {currentPage < lastPage && <div onClick={nextPage}>Next</div>}
-      <PageNumber selected={currentPage === firstPage}>{firstPage}</PageNumber>
+
+      <PageNumber
+        selected={currentPage === firstPage}
+        onClick={() => jumpToPage(firstPage)}
+      >
+        {firstPage}
+      </PageNumber>
       {firstPage + 1 !== pageRange[0] && <p>...</p>}
       {pageRange.map(
         (pageNumber, i): React.ReactNode => (
-          <PageNumber key={i} selected={currentPage === pageNumber}>
+          <PageNumber
+            key={i}
+            selected={currentPage === pageNumber}
+            onClick={() => {
+              jumpToPage(pageNumber);
+            }}
+          >
             {pageNumber}
           </PageNumber>
         )
       )}
       {lastPage - 1 !== pageRange.at(-1) && <p>...</p>}
-      <PageNumber selected={currentPage === lastPage}>{lastPage}</PageNumber>
+      <PageNumber
+        selected={currentPage === lastPage}
+        onClick={() => jumpToPage(lastPage)}
+      >
+        {lastPage}
+      </PageNumber>
+      {currentPage < lastPage && <div onClick={nextPage}>Next</div>}
     </PageNumbersContainer>
   );
 }
--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
+import { useState, useEffect, useMemo, useCallback, useRef } from "react";
 import { range } from "../utils/helpers";
 
 const FIRST_PAGE = 1;
@@ -6,51 +6,64 @@ const FIRST_PAGE = 1;
 function usePagination<T>(
   itemsPerPage: number,
   pageNumbersToShowCount: number = 1,
-  initialPageSize: number
+  initialPageSize: number,
+  dataToPaginate: T[]
 ) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [dataToPaginate, setDataToPaginate] = useState<T[]>([]);
+  const [currentPage, setCurrentPage] = useState(FIRST_PAGE);
   const [currentPageData, setCurrentPageData] = useState<T[]>([]);
   const [pageRange, setPageRange] = useState<number[]>(
     range(FIRST_PAGE + 1, initialPageSize)
   );
 
-  const lastPage = Math.ceil(dataToPaginate.length / itemsPerPage);
+  const currentPageRef = useRef(FIRST_PAGE);
+
+  const lastPage = useMemo(
+    () => Math.ceil(dataToPaginate.length / itemsPerPage),
+    []
+  );
 
   useEffect(() => {
     const getCurrentPageData = () => {
-      const begin = (currentPage - 1) * itemsPerPage;
+      const begin = (currentPageRef.current - 1) * itemsPerPage;
       const end = begin + itemsPerPage;
 
       setCurrentPageData(dataToPaginate.slice(begin, end));
     };
 
     getCurrentPageData();
-  }, [currentPage, dataToPaginate]);
-
-  const updateRangeToShowOnJump = () => {};
+  }, [currentPage]);
 
   const nextPage = useCallback(() => {
+    currentPageRef.current += 1;
     setCurrentPage(currentPage => currentPage + 1);
 
     // update page range
     const pageToChangeRange = initialPageSize - 1; // 4
-    const anotherPageToChangeRange = lastPage - initialPageSize; //6
+    const anotherPageToChangeRange = lastPage - initialPageSize + 1; //6
     if (
-      currentPage >= pageToChangeRange &&
-      currentPage <= anotherPageToChangeRange
+      currentPageRef.current > pageToChangeRange &&
+      currentPageRef.current <= anotherPageToChangeRange
     ) {
       setPageRange(
-        range(currentPage, currentPage + pageNumbersToShowCount + 1)
+        range(
+          currentPageRef.current - 1,
+          currentPageRef.current + pageNumbersToShowCount
+        )
       );
     }
 
-    if (currentPage === anotherPageToChangeRange + 1) {
-      setPageRange(range(currentPage, currentPage + initialPageSize - 2));
+    if (currentPageRef.current === anotherPageToChangeRange + 1) {
+      setPageRange(
+        range(
+          currentPageRef.current - 1,
+          currentPageRef.current + initialPageSize - 3
+        )
+      );
     }
-  }, [currentPage]);
+  }, []);
 
   const prevPage = useCallback(() => {
+    currentPageRef.current -= 1;
     setCurrentPage(currentPage => currentPage - 1);
 
     const pageToChangeRange = initialPageSize - 1; // 4
@@ -58,22 +71,41 @@ function usePagination<T>(
 
     // update page range
     if (
-      currentPage > pageToChangeRange &&
-      currentPage < anotherPageToChangeRange + initialPageSize
+      currentPageRef.current >= pageToChangeRange &&
+      currentPageRef.current < anotherPageToChangeRange + initialPageSize
     ) {
       setPageRange(
-        range(currentPage - pageNumbersToShowCount - 1, currentPage)
+        range(
+          currentPageRef.current - pageNumbersToShowCount,
+          currentPageRef.current + 1
+        )
       );
     }
 
-    if (currentPage === pageToChangeRange + 1) {
-      setPageRange(range(FIRST_PAGE + 1, currentPage));
+    if (currentPageRef.current === pageToChangeRange) {
+      setPageRange(range(FIRST_PAGE + 1, currentPageRef.current + 1));
     }
-  }, [currentPage]);
+  }, []);
 
-  const jumpToPage = (pageNumber: number) => {
+  const jumpToPage = useCallback((pageNumber: number) => {
+    currentPageRef.current = pageNumber;
     setCurrentPage(pageNumber);
-  };
+
+    const anotherPageToChangeRange = lastPage - initialPageSize; //6
+
+    if (currentPageRef.current < initialPageSize) {
+      setPageRange(range(FIRST_PAGE + 1, initialPageSize));
+    } else if (currentPageRef.current > anotherPageToChangeRange + 1) {
+      setPageRange(range(lastPage - initialPageSize + 1, lastPage - 1));
+    } else {
+      setPageRange(
+        range(
+          currentPageRef.current - 1,
+          currentPageRef.current + pageNumbersToShowCount
+        )
+      );
+    }
+  }, []);
 
   return {
     nextPage,
@@ -84,11 +116,7 @@ function usePagination<T>(
     firstPage: FIRST_PAGE,
     lastPage,
     pageRange,
-    setDataToPaginate,
   };
-
-  // hide next button if last page
-  // hide prev button if first page (via styled component props)
 }
 
 export default usePagination;
